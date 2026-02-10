a9c0a9f25da04c79f65dc1cf73fddb39016c3b69
frank99quixoticelixer <francisco.hernandezsuarez01@gmail.com>
2026-02-10 15:40:51 -0600

    Prints data through the terminal, and plots on a graph

--- udp_receiver.py
+++ udp_receiver.py
@@ -1,7 +1,7 @@
 import socket
 import struct
 import sys #for keyboard interrupt
-
+import matplotlib.pyplot as plt #for plots
 UDP_IP = "0.0.0.0"   # listen on all interfaces
 UDP_PORT = 5005
 
@@ -12,6 +12,11 @@ sock.settimeout(0.5) #if for some reason transmission stops, it will exit reciev
 print("Listening for UDP packets...")
 
 counter = 0
+t_data = []
+x_data = []
+y_data = []
+z_data = []
+
 try:
     while True:
         try:
@@ -19,15 +24,31 @@ try:
         except socket.timeout:
             continue #allows The keyboard exception to be "caught" 
     
-        if len(data) == 48:
-            x, y, z, roll, pitch, yaw = struct.unpack('6d', data)
-    
+        if len(data) == 56:
+            t, x, y, z, roll, pitch, yaw = struct.unpack('7d', data)  
+            t_data.append(t)
+            x_data.append(x)
+            y_data.append(y)
+            z_data.append(z)
             counter += 1
             if counter % 10 == 0:   # print at 10 Hz instead of 100 Hz
                 print(f"x={x:.2f}, y={y:.2f}, z={z:.2f}, " f"φ={roll:.2f}, θ={pitch:.2f}, ψ={yaw:.2f}")
         else:
             print(f"Unexpected packet size: {len(data)} bytes")
+
 except KeyboardInterrupt:
     print("\nStopping UDP receiver cleanly...")
     sock.close()
+    # Plot after simulation ends
+    plt.figure()
+    plt.plot(t_data, x_data, label="x")
+    plt.plot(t_data, y_data, label="y")
+    plt.plot(t_data, z_data, label="z")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Position")
+    plt.title("Position vs Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
     sys.exit(0)
